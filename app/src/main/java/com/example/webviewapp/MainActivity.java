4beee07326ba1e5f99089a7b602028491dced840
William Lindholm <a22willi@student.his.se>
2023-04-04 11:00:46 +0200

    Implemented onOptionItemSelected to go to different sites depending on user selection.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -13,12 +13,14 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView myWebView;
+
     public void showExternalWebPage(){
-        // TODO: Add your code for showing external web page here
+        myWebView.loadUrl("https://www.google.com");
     }
 
     public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         WebViewClient myWebViewClient = new WebViewClient();
 
-        WebView myWebView = findViewById(R.id.webview);
+        myWebView = findViewById(R.id.webview);
 
         myWebView.setWebViewClient(myWebViewClient);
 
@@ -80,11 +82,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
